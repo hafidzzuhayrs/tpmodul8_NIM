c05846d2f2cfe7af688fc95d248b3e62a308af74
Hafidz <zuhayrshiddiq16@gmail.com>
2025-04-13 20:34:07 +0700

    initial commit

---
+++ tpmodul8_NIM/Covidconfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+namespace tpmodul8_NIM
+{
+    class Covidconfig
+    {
+        public string satuan_suhu { get; set; } = "celcius";
+        public int batas_hari_deman { get; set; } = 14;
+        public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        private static string filePath = "covid_config.json";
+
+        public static Covidconfig LoadConfig()
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Covidconfig>(json);
+            }
+            else
+            {
+                var defaultConfig = new Covidconfig();
+                defaultConfig.SaveConfig();
+                return defaultConfig;
+            }
+        }
+
+        public void SaveConfig()
+        {
+            string json = JsonConvert.SerializeObject(this, (Newtonsoft.Json.Formatting)System.Xml.Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu.ToLower() == "celcius")
+            {
+                satuan_suhu = "fahrenheit";
+            }
+            else
+            {
+                satuan_suhu = "celcius";
+            }
+            SaveConfig(); // Simpan perubahan ke file
+        }
+    }
+}
--- tpmodul8_NIM/Program.cs
+++ tpmodul8_NIM/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul8_NIM;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Covidconfig config = Covidconfig.LoadConfig();
+
+        // Ganti satuan suhu (G)
+        config.UbahSatuan();
+        Console.WriteLine("Satuan suhu telah diubah menjadi: " + config.satuan_suhu);
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = false;
+
+        if (config.satuan_suhu.ToLower() == "celcius")
+            suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+        else if (config.satuan_suhu.ToLower() == "fahrenheit")
+            suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+
+        if (suhuNormal && hariDemam < config.batas_hari_deman)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
